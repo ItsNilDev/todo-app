3333672e5358f049f8628973c58112c964adfaae
MrPointerDev <poria2100@gmail.com>
2021-07-20 14:03:04 +0430

    v2.0

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
 from rich.console import Console
-from rich.rule import Rule
-import os
-from time import sleep
+from rich.table import Table
 
-# initial setup
-os.system("clear")
-console = Console(width=30)
+table = Table(title="Todo App")
 
-def setup() -> None:
-    os.system("clear")
-    console.print("[bold blue]|", end='')
-    console.print(Rule("TODO", end=''))
-
-    console.print("[bold blue]|  |", end='')
-
-    console.print(Rule("Done", end=''))
-    console.print("[bold blue]|", end='\n  ')
+table.add_column("TODO", justify="center", style="cyan", no_wrap=True)
+table.add_column("DONE", justify="center", style="cyan", no_wrap=True)
 
 TODO_items = []
 DONE_items = []
 
-def update() -> None:
-    # Something is tickling me and telling me that it's not the correct way of doing it...
-    if(DONE_items == []):
-        for i in TODO_items:
-            console.print(f"[ ] {i}", end='\n  ')
-    else:
-        bigger_number = len(TODO_items) if len(TODO_items) > len(DONE_items) \
-                        else len(DONE_items)
-        for i in range(0, bigger_number):
-            try:
-                console.print(f"[ ] {TODO_items[i]}", end='                         ')
-            except:
-                console.print("        ", end='       ')
-                console.print("        ", end='       ')
-            console.print(f"[X] {DONE_items[i]}", end='\n  ')
-
-# Add item to each list
 def add_todo_item(title: str) -> None:
     TODO_items.append(title)
-    setup()
-    update()
 
 def add_done_item(title: str) -> None:
     DONE_items.append(title)
-    setup()
-    update()
+    # table.add_row("", f"[X] {title}")
 
-def delete_todo_item(title: str) -> None:
-    TODO_items.remove(title)
-    setup()
-    update()
+def update() -> None:
+    bigger_number = len(TODO_items) if len(TODO_items) > len(DONE_items) else len(DONE_items)
+    for i in range(0, bigger_number):
+        print(i)
+        try: 
+            table.add_row(f"[ ] {TODO_items[i]}", f"[X] {DONE_items[i]}")
+        except:
+            if(len(TODO_items) > len(DONE_items)):
+                table.add_row(f"[ ] {TODO_items[i]}", "")       
+            else:
+                table.add_row("", f"[X] {DONE_items[i]}")       
 
-# def move_to_done(title: str) -> None:
-#     delete_todo_item(title)
+        
 
 
-setup()
-add_todo_item("Prove that 2+2 is not 5")
-add_done_item("Add a version control")
-add_done_item("Make this project public")
-add_todo_item("Buy milk")
+
+add_done_item("buy milk")
+add_done_item("Use a version control")
+add_done_item("Use a version control")
+add_todo_item("Lorem ipsum dolor sit ametconsectetur")
+add_done_item("Use a version control")
+add_todo_item("adipiscing elit consectetur adipiscing")
+add_todo_item("prove that 2+2 is not 5")
+update()
+
+console = Console()
+console.print(table)
